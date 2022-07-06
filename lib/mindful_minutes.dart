54fb6105ad63b6b1039f58453314bec14e192369
nohli <43643339+nohli@users.noreply.github.com>
2022-07-06 20:26:10 +0200

    Add return type to method calls

--- lib/mindful_minutes.dart
+++ lib/mindful_minutes.dart
@@ -6,21 +6,22 @@ class MindfulMinutesPlugin {
   static const MethodChannel _channel = MethodChannel('mindful_minutes');
 
   Future<bool> checkPermission() async {
-    return await _channel.invokeMethod('checkPermission') ?? false;
+    return await _channel.invokeMethod<bool?>('checkPermission') ?? false;
   }
 
   Future<bool> requestPermission() async {
-    return await _channel.invokeMethod('requestPermission') ?? false;
+    return await _channel.invokeMethod<bool?>('requestPermission') ?? false;
   }
 
   Future<bool> writeMindfulMinutes(
     DateTime startTime,
     DateTime endTime,
   ) async {
-    Map<String, dynamic> args = {
+    Map<String, int> args = {
       'startTime': startTime.millisecondsSinceEpoch,
       'endTime': endTime.millisecondsSinceEpoch
     };
-    return await _channel.invokeMethod('saveMindfulMinutes', args) ?? false;
+    return await _channel.invokeMethod<bool?>('saveMindfulMinutes', args) ??
+        false;
   }
 }
