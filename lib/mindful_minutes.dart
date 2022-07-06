a3b17aef2ea2f751461966e31bac6b973426bf1b
nohli <43643339+nohli@users.noreply.github.com>
2022-07-06 20:30:45 +0200

    Document public API members

--- lib/mindful_minutes.dart
+++ lib/mindful_minutes.dart
@@ -2,17 +2,24 @@ library mindful_minutes;
 
 import 'package:flutter/services.dart';
 
+/// The class for writing mindful minutes to Apple Health.
 class MindfulMinutesPlugin {
   static const MethodChannel _channel = MethodChannel('mindful_minutes');
 
+  /// Checks if the app has writing permissions for mindful minutes.
+  /// Returns false if the call was not successful.
   Future<bool> checkPermission() async {
     return await _channel.invokeMethod<bool?>('checkPermission') ?? false;
   }
 
+  /// Checks if the app has writing permissions for mindful minutes.
+  /// Returns false if the call was not successful.
   Future<bool> requestPermission() async {
     return await _channel.invokeMethod<bool?>('requestPermission') ?? false;
   }
 
+  /// Writes mindful minutes to Apple Health.
+  /// Returns false if the call was not successful.
   Future<bool> writeMindfulMinutes(
     DateTime startTime,
     DateTime endTime,
