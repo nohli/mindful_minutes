b63e056ffe3d23071a5f391a4d7a73c40cc0eeb0
Joachim Nohl <43643339+nohli@users.noreply.github.com>
2023-08-14 03:31:44 +0200

    Refactor native code, add entitlements to example

--- ios/Classes/MindfulMinutesPlugin.swift
+++ ios/Classes/MindfulMinutesPlugin.swift
@@ -1,74 +1,83 @@
 import Flutter
-import UIKit
 import HealthKit
 
 public class MindfulMinutesPlugin: NSObject, FlutterPlugin {
-    
-    let healthStore = HKHealthStore()
-    let type = HKSampleType.categoryType(forIdentifier: .mindfulSession)!
-    
-    struct PluginError: Error {
-        let message: String
+
+  let healthStore = HKHealthStore()
+  let type = HKSampleType.categoryType(forIdentifier: .mindfulSession)!
+
+  public static func register(with registrar: FlutterPluginRegistrar) {
+    // Check if HealthKit is available on the device
+    if !HKHealthStore.isHealthDataAvailable() {
+      print("HealthKit is not available on this device.")
+      return
     }
-    
-    public static func register(with registrar: FlutterPluginRegistrar) {
-        let channel = FlutterMethodChannel(name: "mindful_minutes", binaryMessenger: registrar.messenger())
-        let instance = MindfulMinutesPlugin()
-        registrar.addMethodCallDelegate(instance, channel: channel)
+
+    let channel = FlutterMethodChannel(name: "mindful_minutes", binaryMessenger: registrar.messenger())
+    let instance = MindfulMinutesPlugin()
+    registrar.addMethodCallDelegate(instance, channel: channel)
+  }
+
+  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    switch call.method {
+    case "checkPermission":
+      checkPermission(call: call, result: result)
+    case "requestPermission":
+      requestPermission(call: call, result: result)
+    case "saveMindfulMinutes":
+      saveMindfulMinutes(call: call, result: result)
+    default:
+      result(FlutterMethodNotImplemented)
     }
-    
-    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        switch (call.method) {
-        case "checkPermission":
-            try! _checkPermission(call: call, result: result)
-            break;
-        case "requestPermission":
-            try! _requestPermission(call: call, result: result)
-            break;
-        case "saveMindfulMinutes":
-            try! _saveMindfulMinutes(call: call, result: result)
-            break;
-        default:
-            break;
-        }
+  }
+
+  // Function to check if permission has been granted for the app to write to HealthKit
+  private func checkPermission(call: FlutterMethodCall, result: @escaping FlutterResult) {
+    let status = healthStore.authorizationStatus(for: type)
+    let granted = status == HKAuthorizationStatus.sharingAuthorized
+    DispatchQueue.main.async {
+      result(granted)
     }
-    
-    func _checkPermission(call: FlutterMethodCall, result: @escaping FlutterResult) throws {
-        let status = healthStore.authorizationStatus(for: type)
-        let granted = status == HKAuthorizationStatus.sharingAuthorized
+  }
+
+  // Function to request permission from the user to write to HealthKit
+  private func requestPermission(call: FlutterMethodCall, result: @escaping FlutterResult) {
+    healthStore.requestAuthorization(toShare: [type], read: nil) { (success, error) in
+      if let error = error {
         DispatchQueue.main.async {
-            result(granted)
+          result(FlutterError(code: "PERMISSION_ERROR", message: error.localizedDescription, details: nil))
         }
+        return
+      }
+      DispatchQueue.main.async {
+        result(success)
+      }
     }
-    
-    func _requestPermission(call: FlutterMethodCall, result: @escaping FlutterResult) throws {
-        healthStore.requestAuthorization(toShare: [type], read: nil) { (success, error) in
-            DispatchQueue.main.async {
-                result(success)
-            }
+  }
+
+  // Function to save mindful minutes to HealthKit
+  private func saveMindfulMinutes(call: FlutterMethodCall, result: @escaping FlutterResult) {
+    guard let arguments = call.arguments as? NSDictionary,
+          let startTime = arguments["startTime"] as? NSNumber,
+          let endTime = arguments["endTime"] as? NSNumber else {
+        result(FlutterError(code: "INVALID_ARGUMENTS", message: "Invalid arguments for saveMindfulMinutes", details: nil))
+        return
+    }
+
+    let start = Date(timeIntervalSince1970: startTime.doubleValue / 1000)
+    let end = Date(timeIntervalSince1970: endTime.doubleValue / 1000)
+    let sample = HKCategorySample(type: type, value: 0, start: start, end: end)
+
+    healthStore.save(sample) { (success, error) in
+      if let error = error {
+        DispatchQueue.main.async {
+          result(FlutterError(code: "SAVE_ERROR", message: "Error saving \(self.type): \(error.localizedDescription)", details: nil))
         }
+        return
+      }
+      DispatchQueue.main.async {
+        result(success)
+      }
     }
-    
-    func _saveMindfulMinutes(call: FlutterMethodCall, result: @escaping FlutterResult) throws {
-        guard let arguments = call.arguments as? NSDictionary,
-              let startTime = arguments["startTime"] as? NSNumber,
-              let endTime = arguments["endTime"] as? NSNumber
-        else {
-            throw PluginError(message: "Invalid Arguments")
-        }
-        
-        let start = Date(timeIntervalSince1970: startTime.doubleValue / 1000)
-        let end = Date(timeIntervalSince1970: endTime.doubleValue / 1000)
-        let sample = HKCategorySample(type: type, value: 0, start: start, end: end)
-        
-        healthStore.save(sample, withCompletion: { (success, error) in
-            if let err = error {
-                print("Error saving \(self.type).")
-                print("Sample: \(err.localizedDescription).")
-            }
-            DispatchQueue.main.async {
-                result(success)
-            }
-        })
-    }
+  }
 }
